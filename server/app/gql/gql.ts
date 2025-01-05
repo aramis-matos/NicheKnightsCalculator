4eef1580b125bff57d255e6488b50fb59b4c279f
aramis-matos <aramis.matos1@gmail.com>
2025-01-05 08:48:54 -0500

    changed allClasses call

--- server/app/gql/gql.ts
+++ server/app/gql/gql.ts
@@ -17,7 +17,7 @@ const documents = {
     "query allArtists {\n  allArtists {\n    nodes {\n      rowId\n      name\n    }\n  }\n}": types.AllArtistsDocument,
     "query allBranches {\n  allBranches {\n    nodes {\n      id\n      name\n    }\n  }\n}": types.AllBranchesDocument,
     "query allCharacterArtist($search: String!) {\n  allCharacterArtist(search: $search) {\n    nodes {\n      id\n      name\n    }\n  }\n}": types.AllCharacterArtistDocument,
-    "query allClasses {\n  allClasses {\n    nodes {\n      id\n      name\n    }\n  }\n}": types.AllClassesDocument,
+    "query allClasses {\n  allClasses {\n    nodes {\n      name\n    }\n  }\n}": types.AllClassesDocument,
     "query allGenders {\n  allGenders {\n    nodes {\n      id\n      name\n    }\n  }\n}": types.AllGendersDocument,
     "query allGeneralSearch($search: String) {\n  allGeneralSearch(search: $search) {\n    nodes {\n      id\n      name\n      classByClassId {\n        name\n      }\n    }\n  }\n}": types.AllGeneralSearchDocument,
     "query charactersLessThanHeight($height: Int = 135) {\n  allHeightCharacter(charHeight: $height) {\n    nodes {\n      id\n      name\n    }\n  }\n}\n\nquery allRaceCharacter($race: String) {\n  allRaceCharacter(search: $race) {\n    nodes {\n      id\n      name\n    }\n  }\n}\n\nquery allCharacter($search: String) {\n  allCharacters(condition: {name: $search}) {\n    nodes {\n      id\n      name\n      classByClassId {\n        name\n      }\n    }\n  }\n}\n\nquery getOperatorDetails($name: String!) {\n  characterByName(name: $name) {\n    name\n    height\n    placeOfBirthByPlaceOfBirthId {\n      name\n    }\n    raceByRaceId {\n      name\n    }\n    infectionByInfectionId {\n      name\n    }\n    classByClassId {\n      name\n    }\n    branchByBranchId {\n      name\n    }\n    characterArtistsByCharacterId {\n      nodes {\n        artistByArtistId {\n          name\n        }\n      }\n    }\n    genderByGenderId {\n      name\n    }\n  }\n}": types.CharactersLessThanHeightDocument,
@@ -52,7 +52,7 @@ export function graphql(source: "query allCharacterArtist($search: String!) {\n 
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query allClasses {\n  allClasses {\n    nodes {\n      id\n      name\n    }\n  }\n}"): (typeof documents)["query allClasses {\n  allClasses {\n    nodes {\n      id\n      name\n    }\n  }\n}"];
+export function graphql(source: "query allClasses {\n  allClasses {\n    nodes {\n      name\n    }\n  }\n}"): (typeof documents)["query allClasses {\n  allClasses {\n    nodes {\n      name\n    }\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
