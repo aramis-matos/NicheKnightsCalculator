28d787b644b364110a1784592591fc32768c60fa
aramis-matos <aramis.matos1@gmail.com>
2025-01-04 20:29:44 -0500

    gonna refactor select to checkbox because it looks stupid

--- server/app/components/Select.vue
+++ server/app/components/Select.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-defineProps<{ title: string; options?: string[] }>();
+defineProps<{
+  title: string;
+  options?: { id: string; name: string }[];
+}>();
 </script>
 
 <template>
-  <label class="form-control w-full max-w-xs">
+  <label class="form-control w-full">
     <div class="label">
       <span class="label-text">{{ title }}</span>
     </div>
     <select class="select select-bordered" multiple="true">
       <option disabled selected>Pick multiple</option>
-      <option v-for="op of options" :value="op">{{ op }}</option>
+      <option v-for="op of options" :value="op" :key="op.id">
+        {{ op.name }}
+      </option>
     </select>
   </label>
 </template>
--- server/app/pages/niche/index.vue
+++ server/app/pages/niche/index.vue
@@ -1,3 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { AllClassesQuery } from "~/gql/graphql";
 
-<template></template>
+const classes = reactive<AllClassesQuery>({});
+
+const allClassesGql = useGqlWithTypes<AllClassesQuery>("allClasses");
+
+classes.allClasses = (
+  await allClassesGql.useAsyncGql({})
+).data.value.allClasses;
+</script>
+
+<template>
+  <div class="flex w-full items-center justify-center">
+    <div class="grid w-full max-w-fit grid-cols-2 md:grid-cols-3">
+      <Select
+        title="Classes"
+        :options="classes.allClasses?.nodes.filter((elem) => elem !== null)"
+      />
+    </div>
+  </div>
+</template>
