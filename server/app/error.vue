772ab5d622534e777d4f386836a4db990628b9da
aramis-matos <aramis.matos1@gmail.com>
2025-01-11 08:31:26 -0500

    fixed error codes

--- server/app/error.vue
+++ server/app/error.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import type { NuxtError } from "nuxt/app";
+
+defineProps({ error: Object as () => NuxtError });
+</script>
 <template>
   <NuxtLayout>
-    <div class="prose dark:prose-invert">
-      <ContentDoc path="error" />
-    </div>
+    <UCard>
+      <div class="prose dark:prose-invert">
+        <h2>Error - {{ error?.statusCode }}</h2>
+        <p>{{ error?.message }}</p>
+        <p>Go back <ULink to="/">home</ULink></p>
+      </div>
+    </UCard>
   </NuxtLayout>
 </template>
